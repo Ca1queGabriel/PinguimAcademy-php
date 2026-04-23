ab3060a8ee6d9ace50bcadcc0867296c1175ccf3
Robinson <engenharia@galli.com.br>
2026-04-23 16:10:27 -0300

    Adicionando arquivos de configuração e ignorando arquivos desnecessários

--- PhpFundamentos/30-12/include.php
+++ PhpFundamentos/30-12/include.php
@@ -1,12 +1,26 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <?php include 'head.php' ?>
+    <?php include('inc/head.php') ?>
+   <?php $config  = include('inc/config.php') ?>
     <title>Include</title>
 </head>
 <body>
     <div>
         <h1><?= 'Include' ?></h1>
+
+        <table>
+            <thead>
+            <th>Usuário</th>
+            <th>Senha</th>
+            </thead>
+            <tbody>
+            <tr>
+                <td><?= $config['username'] ?></td>
+                <td><?= $config['password'] ?></td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
