78dd44cd79ec3fdf25028aac2b119e7b80a6e2ce
Robinson <engenharia@galli.com.br>
2026-04-23 15:42:40 -0300

    Adicionando arquivos head.php, include.php e index.php com estrutura básica de HTML

---
+++ PhpFundamentos/30-12/include.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include 'head.php' ?>
+    <title>Include</title>
+</head>
+<body>
+    <div>
+        <h1><?= 'Include' ?></h1>
+    </div>
+</body>
+</html>
---
+++ PhpFundamentos/30-12/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Include e Require</title>
+</head>
+<body>
+        <h1>Include e Require</h1>
+        <div>
+            <ul>
+                <li>
+                    <a href="include.php">Include</a>
+                </li>
+                <li>
+                    <a href="/require.php">Require</a>
+                </li>
+            </ul>
+        </div>
+</body>
+</html>
