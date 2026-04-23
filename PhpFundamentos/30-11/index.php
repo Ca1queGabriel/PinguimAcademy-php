78edbe0bcdbefd5c410145bc821d002606e1b58f
Robinson <engenharia@galli.com.br>
2026-04-23 14:52:51 -0300

    Deixando o visual da aula 30-11 mais agradável (literal só colocando em lista alguns elementos)

--- PhpFundamentos/30-11/index.php
+++ PhpFundamentos/30-11/index.php
@@ -6,17 +6,21 @@
     <title>Estruturas de Repetição</title>
 </head>
 <body>
-    <div>
-        <h2 style ="margin-top: 50px">Estrutura for</h2>
-        <a href="for.php">Estrutura for</a>
-    </div>
-    <div>
-        <h2 style ="margin-top: 50px">Estrutura foreach</h2>
-        <a href="foreach.php">Estrutura foreach</a>
-    </div>
-    <div>
-        <h2 style ="margin-top: 50px">Estrutura while</h2>
-        <a href="while.php">Estrutura while</a>
-    </div>
+<div>
+    <ul>
+        <li>
+          <a href="for.php">Estrutura for</a>
+        </li>
+        <li>
+            <a href="foreach.php">Estrutura foreach</a>
+        </li>
+        <li>
+            <a href="while.php">Estrutura while</a>
+        </li>
+        <li>
+            <a href="while.php">Estrutura while</a>
+        </li>
+    </ul>
+</div>
 </body>
 </html>
